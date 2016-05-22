b31dc7a88ecd8344145eafbd461169c2a5718801
Eivar Montenegro <e.mont01@gmail.com>
2016-05-22 16:53:34 -0500

    adding tests for the given score examples

--- src/test/scala/com/tweetsec/challenge/PasswordStrengthEvaluatorSpec.scala
+++ src/test/scala/com/tweetsec/challenge/PasswordStrengthEvaluatorSpec.scala
@@ -23,4 +23,28 @@ class PasswordStrengthEvaluatorSpec extends UnitSpec {
     
     actual.processedText should equal(expected)
   }
+  
+  it should "assign the correct score for 'password1' --> 4" in {
+    val expected = 4
+    val passwordScore = PasswordStrengthEvaluator.eval("password1")
+    val actual = passwordScore.value
+    
+    assert(actual == expected)
+  }
+  
+  it should "assign the correct score for 'goat m4n' --> 15" in {
+    val expected = 15
+    val passwordScore = PasswordStrengthEvaluator.eval("goat m4n")
+    val actual = passwordScore.value
+    
+    assert(actual == expected)
+  }
+  
+  it should "assign the correct score for 's0_0per 5n4k3' --> 44" in {
+    val expected = 44
+    val passwordScore = PasswordStrengthEvaluator.eval("s0_0per 5n4k3")
+    val actual = passwordScore.value
+    
+    assert(actual == expected)
+  }
 }
