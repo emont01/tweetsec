b0983d0904a8a9f46f70510012b7f40272a20e4c
Eivar Montenegro <e.mont01@gmail.com>
2016-05-24 01:33:48 -0500

    creating spec for YouShallNotPassword

--- src/test/scala/com/tweetsec/challenge/YouShallNotPasswordSpec.scala
+++ src/test/scala/com/tweetsec/challenge/YouShallNotPasswordSpec.scala
@@ -6,19 +6,36 @@
 
 package com.tweetsec.challenge
 
+import twitter4j.Twitter
+import org.mockito.Mockito._
+
 class YouShallNotPasswordSpec extends UnitSpec {
+
   behavior of "YouShallNotPassword"
-  
-  it should "Strong passwords should receive a congratulatory tweet in response" in {
-    //
+
+  it should "send a congratulatory tweet in response for strong passwords" in {
+    val m = mock[Twitter]
+    val app = new YouShallNotPassword(m)
+    app.eval("@JohnDoe", "4 v3ry str0ng passw0rd.")
+    
+    verify(m).updateStatus("Congratulations @JohnDoe '4 v3ry str0ng passw0rd.' is a strong password!")
   }
-  
-  it should "Weak passwords should receive a strengthened version of the original password" in {
+
+  it should "send a strengthened version of the original password for weak passwords" in {
     //The modified version should be no longer than the original, unless it must 
     //be lengthened to qualify as strong.
+    val m = mock[Twitter]
+    val app = new YouShallNotPassword(m)
+    app.eval("@JohnDoe", "s0_0per 5n4k3")
+    
+    verify(m).updateStatus("@JohnDoe 's0_0per 5n4k3' is a weak password, try with 's0_0p3r 5n4k3'")
   }
-  
-  it should "Unacceptable passwords should receive a response suggesting that the sender try again with a better password." in {
-    //
+
+  it should "send a response suggesting that the sender try again with a better password." in {
+    val m = mock[Twitter]
+    val app = new YouShallNotPassword(m)
+    app.eval("@JohnDoe", "password1")
+    
+    verify(m).updateStatus("@JohnDoe unacceptable password, please try again with a better password!")
   }
 }
