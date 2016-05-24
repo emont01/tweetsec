bd000c901afe4d77d7f02222a1475c92e9b0c3cd
Eivar Montenegro <e.mont01@gmail.com>
2016-05-24 01:33:16 -0500

    adding support for a mocking framework

--- src/test/scala/com/tweetsec/challenge/UnitSpec.scala
+++ src/test/scala/com/tweetsec/challenge/UnitSpec.scala
@@ -1,6 +1,8 @@
 package com.tweetsec.challenge
 
 import org.scalatest._
+import org.scalatest.mock.MockitoSugar
 
 abstract class UnitSpec extends FlatSpec with Matchers
   with OptionValues with Inside with Inspectors
+  with MockitoSugar
